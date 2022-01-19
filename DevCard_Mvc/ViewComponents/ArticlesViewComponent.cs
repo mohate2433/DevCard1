c339ae77e0640987a1e547a1d986ca7516dc1ac3
Mohammad Rezaei <mohate2433@gmail.com>
2022-01-19 23:07:32 +0330

    ViewComponents Article & Project

---
+++ DevCard_Mvc/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard_Mvc.Models;
+
+namespace DevCard_Mvc.ViewComponents
+{
+    public class ArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var Article = new List<Article>
+            {
+                new Article(1,"رستوران","پخت انواع غذاهای ایرانی","blog-post-thumb-card-1.jpg"),
+                new Article(2,"zapya","نرم افزاری برای انتقال داده ها","blog-post-thumb-card-2.jpg"),
+                new Article(3,"فیلیمو","برنامه ای برای انواع فیلم ها","blog-post-thumb-card-3.jpg"),
+            };
+            return View("_article",Article);
+        }
+    }
+}
---
+++ DevCard_Mvc/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard_Mvc.Models;
+
+namespace DevCard_Mvc.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var Project = new List<Project>
+            {
+                new Project(1,"تاکسی","تاکسی درون شهری","blog-post-thumb-1.jpg","snap"),
+                new Project(2,"اتوبوس","اتوبوس درون شهری","blog-post-thumb-2.jpg","vahed"),
+                new Project(3,"فست قود","انواع پیتزا و ساندویچ","blog-post-thumb-3.jpg","zapa"),
+                new Project(4,"الکل","انواع الکل های طبی","blog-post-thumb-4.jpg","taviar"),
+            };
+            return View("_project",Project);
+        }
+    }
+}
